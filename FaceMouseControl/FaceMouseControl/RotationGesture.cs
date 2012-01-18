8629dcd13e724ed284bb29e12f44dcd2a1cd19a9
mateusz.chrominski@gmail.com <mateusz.chrominski@gmail.com@3418511e-35c4-3ee4-f4f9-b7d248f41474>
2012-01-18 19:39:38 +0000

    MIU-1 Project initiation

---
+++ FaceMouseControl/FaceMouseControl/RotationGesture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FaceController
+{
+    public class RotationGesture
+    {
+        private const int TRESHOLD = 20;
+
+        public RotationGesture(FaceRecognition recognizer)
+        {
+            recognizer.EventListeners += new FrameReceiverFunc(Compute);
+        }
+
+        public void Compute(FrameData data)
+        {
+            if (data.Rotation < -TRESHOLD)
+            {
+                Console.WriteLine("LEFT rotation");
+            }
+            if (data.Rotation > TRESHOLD)
+            {
+                Console.WriteLine("RIGHT rotation");
+            }
+        }
+    }
+}
